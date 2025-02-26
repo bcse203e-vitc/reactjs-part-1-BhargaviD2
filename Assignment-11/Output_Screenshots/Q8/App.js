8c53d11091f29a6d6d8045421782e25611e3c8a6
BhargaviD2 <166976522+BhargaviD2@users.noreply.github.com>
2025-02-26 12:06:02 +0530

    Create App.js

---
+++ Assignment-11/Output_Screenshots/Q8/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+function App() {
+
+  function clicker(){
+    alert("Button Clicked!");
+  }
+  return(
+    <button onClick={clicker}>Click Me!</button>
+  )
+
+}
+
+export default App;
